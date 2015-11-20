02e5a39c36d7b3e962a039a64c1dc07a6e56563a
andolojm <jeeeeefff@gmail.com>
2015-11-19 20:23:24 -0500

    Prevent PHP file uploads

--- views/upload-claim.php
+++ views/upload-claim.php
@@ -1,11 +1,14 @@
 <?php if(isset($_POST['submission'])){
     $uploaddir = './collision-img/';
     $uploadfile = $uploaddir . basename($_FILES['photo']['name']);
-    if (move_uploaded_file($_FILES['photo']['tmp_name'], $uploadfile)) { ?>
-        <p>Successssss</p>
-    <?php } else { ?>
-        <p>Errored, try again with less awful data</p>
-    <?php }
+    echo pathinfo($uploadfile, PATHINFO_EXTENSION);
+    if (pathinfo($uploadfile, PATHINFO_EXTENSION) == 'php'){
+        if (move_uploaded_file($_FILES['photo']['tmp_name'], $uploadfile)) { ?>
+            <p>Successssss</p>
+        <?php } else { ?>
+            <p>Errored, try again with less awful data</p>
+        <?php }
+    }
 } else { ?>
     <form enctype="multipart/form-data" method="POST">
         <input type="file" name="photo" />

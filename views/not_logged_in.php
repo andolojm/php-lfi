8e91c109b7bad45cae9b22bcaed17cad9367e7ca
andolojm <jeeeeefff@gmail.com>
2015-11-26 09:24:26 -0500

    Begin styling

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -1,3 +1,5 @@
+<body>
+
 <?php
 // show potential errors / feedback (from login object)
 if (isset($login)) {
@@ -13,19 +15,26 @@ if (isset($login)) {
     }
 }
 ?>
-
-<!-- login form box -->
-<form method="post" action="index.php" name="loginform">
-
-    <label for="login_input_username">Username</label>
-    <input id="login_input_username" class="login_input" type="text" name="user_name" required />
-
-    <label for="login_input_password">Password</label>
-    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
-
-    <input type="submit"  name="login" value="Log in" />
-
-</form>
-
-<a href="register.php">Register new account ADMINS ONLY PLS</a>
-<a href="forgot.php">I'm a dick, and forgot my password</a>
+<div class="container">
+    <div class="masthead">
+        <h3 class="text-muted">Sudo Auto Insurance</h3>
+        <nav>
+            <ul class="nav nav-justified">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Submit a Claim</a></li>
+                <li>
+                    <form method="post" action="index.php" name="loginform">
+                        <input id="login_input_username" size="9" placeholder="Username" class="login_input" type="text" name="user_name" required />
+                        <input id="login_input_password" size="9" placeholder="Password" class="login_input" type="password" name="user_password" autocomplete="off" required />
+                        <input type="submit"  name="login" value="Log in" />
+                    </form>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <!-- login form box -->
+        <a href="register.php">Register new account ADMINS ONLY PLS</a>
+    <a href="forgot.php">I'm a dick, and forgot my password</a>
+</div>
+</body>
+</html>
--- views/upload-claim.php
+++ views/upload-claim.php
@@ -1,14 +1,16 @@
 <?php if(isset($_POST['submission'])){
     $uploaddir = './collision-img/';
-    $uploadfile = $uploaddir . basename($_FILES['photo']['name']);
-    echo pathinfo($uploadfile, PATHINFO_EXTENSION);
-    if (pathinfo($uploadfile, PATHINFO_EXTENSION) != 'php'){
+    $filename = basename($_FILES['photo']['name']);
+    $uploadfile = $uploaddir . $filename;
+    if (strpos($filename, 'jpg') !== false){
         if (move_uploaded_file($_FILES['photo']['tmp_name'], $uploadfile)) { ?>
             <p>Successssss</p>
         <?php } else { ?>
             <p>Errored, try again with less awful data</p>
         <?php }
-    }
+    } else { ?>
+        <p> LOL U TRYNA HACK </p>  
+<?php }
 } else { ?>
     <form enctype="multipart/form-data" method="POST">
         <input type="file" name="photo" />
